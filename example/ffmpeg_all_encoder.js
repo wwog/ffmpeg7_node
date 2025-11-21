4839d49a891ed07eeb4899a60714457d5b7439e7
wwog <871782513@qq.com>
2025-11-21 16:25:49 +0800

    -

--- example/ffmpeg_all_encoder.js
+++ example/ffmpeg_all_encoder.js
@@ -2,6 +2,8 @@ const ffmpeg = require('../dist/index.js');
 const path = require('path');
 
 // ffmpeg.run(['-encoders']);
-const inputFile = path.join(__dirname, 'input.mp4');
-const outputFile = path.join(__dirname, 't.png')
-ffmpeg.run(['-i', inputFile, '-vf', 'thumbnail', '-frames:v', "1", outputFile]);
+// const inputFile = path.join(__dirname, 'test.mp4');
+// const outputFile = path.join(__dirname, 't.png')
+// const args = ['-i', inputFile, '-vf', 'thumbnail', '-frames:v', "1", outputFile]
+// console.log(args.join(' '))
+// ffmpeg.run(args);
